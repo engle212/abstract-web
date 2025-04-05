102960b74dfe04ed825f9e7c7772b849c1cd97c8
John Engle <englejohn64@gmail.com>
2025-04-05 14:18:29 -0400

    Initialized Spring app

---
+++ src/main/java/com/example/demo/AbstractApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbstractApplication.class, args);
+	}
+
+}
