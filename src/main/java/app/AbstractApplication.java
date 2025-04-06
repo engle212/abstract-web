d42301315d333884d05f8158c12f5bdb045a93b4
John Engle <englejohn64@gmail.com>
2025-04-06 13:23:41 -0400

    Separated controller from app

--- src/main/java/app/AbstractApplication.java
+++ src/main/java/app/AbstractApplication.java
@@ -2,17 +2,9 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class AbstractApplication {
-
-	@RequestMapping("/")
-	public String home() {
-		return "Hello world!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AbstractApplication.class, args);
