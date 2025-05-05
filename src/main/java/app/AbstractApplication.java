092168e67b916b4d5e85d7efda7d9a5b3067f2f0
John Engle <englejohn64@gmail.com>
2025-05-05 14:06:37 -0400

    Implemented OAuth log-in

--- src/main/java/app/AbstractApplication.java
+++ src/main/java/app/AbstractApplication.java
@@ -4,10 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AbstractApplication {
+public class AbstractApplication /*extends WebSecurityConfigurerAdapter*/ {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AbstractApplication.class, args);
 	}
+	/*
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http.authorizeRequests(a -> a
+				.antMatchers("/", "/error", "/webjars/**").permitAll()
+				.anyRequest().authenticated()
+		).exceptionHandling(e -> e
+				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+		).oauth2Login();
+		// @formatter:on
+	}*/
 
 }
