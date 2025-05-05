e16c28b29d7f72618c5457aa89f9b9c2bcbb00ef
John Engle <englejohn64@gmail.com>
2025-05-05 14:13:32 -0400

    Implemented OAuth log-in

--- src/main/java/app/AbstractController.java
+++ src/main/java/app/AbstractController.java
@@ -6,12 +6,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Collections;
+import java.util.Map;
+
 @Controller
 public class AbstractController {
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("testString", "Index");
+    public String index(/*Model model*/) {
+        //model.addAttribute("testString", "Index");
         return "index";
     }
+
+    @GetMapping("/secured")
+    public String secured(/*Model model*/) {
+        //model.addAttribute("testString", "Index");
+        return "secure page!";
+    }
+
+    /*@GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }*/
 
 }
